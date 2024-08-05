2f886c4a6a1b0f9dbb3189326b2a045bb9a66b87
Nguyen <tnguyen9@teksystems.com>
2024-08-05 14:02:38 -0500

    added an option to create users from the RITM ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from placeholder import Login, ScrapeRITM
+from placeholder import Login, ScrapeRITM, UserCreation
 from acc import get_accs
 
 if __name__ == '__main__':
@@ -16,21 +16,34 @@ if __name__ == '__main__':
     login = Login(driver, login_link, user, pw)
     login.login_sn()
 
+    # TODO: create a while loop
+    # TODO: input to choose either user creation or label generation (WIP)
     # search for an RITM ticket and scrape the info on the ticket
-    # TODO: create a proper loop, for now keep break
     # xpath for global search in order to enter the RITM number
-    search_xpath = '//input[@name="sysparm_search"]'
-    while True:
+    new_user_link = 'https://tek.service-now.com/nav_to.do?uri=%2Fsys_user.do%3Fsys_id%3D-1%26sys_is_list%3Dtrue%26sys_target%3Dsys_user%26sysparm_checked_items%3D%26sysparm_fixed_query%3D%26sysparm_group_sort%3D%26sysparm_list_css%3D%26sysparm_query%3DGOTO123TEXTQUERY321%3DDavid%2BKvachev%26sysparm_referring_url%3Dsys_user_list.do%3Fsysparm_query%3DGOTO123TEXTQUERY321%253DDavid%2BKvachev@99@sysparm_first_row%3D1%26sysparm_target%3D%26sysparm_view%3D'
+    
+    # temp variable for testing, remove later
+    count = 0
+    while count < 2:
         ritm = input("Enter an RITM number: ")
-        scraper = ScrapeRITM(driver, ritm, search_xpath)
+        scraper = ScrapeRITM(driver, ritm)
         scraper.search_ritm()
 
         req, name, address = scraper.scrape_ritm()
-
         # remove this later, used for debugging purposes
         print(f"{ritm} {req} {name} {address}")
 
-        input("Enter any key to continue")
-        break
+        user_info = scraper.scrape_user_info()
+        # remove this later, used for debugging purposes
+        print(user_info)
+
+        count += 1
+        input("Enter 'enter' to continue")
+        
+        new_user = UserCreation(driver, new_user_link, user_info, name)
+        new_user.format_office_id()
+        new_user.create_user()
+
+        input("Enter 'enter' to continue")
 
     driver.quit()
--- placeholder.py
+++ placeholder.py
@@ -13,6 +13,7 @@ class Login:
         self.driver.get(self.link)
 
         time.sleep(5)
+        print("Logging in...")
         
         self.driver.switch_to.frame("gsft_main")
 
@@ -28,14 +29,27 @@ class Login:
         time.sleep(8)
 
 class ScrapeRITM():
-    def __init__(self, driver, ritm, search_xpath):
+    def __init__(self, driver, ritm):
         self.driver = driver
         self.ritm = ritm
-        self.search_xpath = search_xpath
+
+        # global search xpath
+        self.search_xpath = '//input[@name="sysparm_search"]'
+
+        # xpath of RITM tickets
+        # these are containers that hold other xpaths for form values
+        self.consultant_info_xpath = '//tr[@id="element.container_23caec60e17c4a00c2ab91d15440c5ee"]'
+        self.address_info_xpath = '//tr[@id="element.container_66291a0ae1fc8a00c2ab91d15440c5c2"]'
+        self.company_info_xpath = '//tr[@id="element.container_84f76a0ee1fc8a00c2ab91d15440c50e"]'
+        # normal xpaths inside a ticket
+        self.req_xpath = '//input[@id="sys_display.sc_req_item.request"]'
     
     # main functionality, enter the RITM value into the global search
     # in order to grab the values
     def search_ritm(self):
+        # ensure that driver is not in a frame before performing a search.
+        self.driver.switch_to.default_content()
+        
         # search for global search bar and query the site for an RITM ticket
         global_search = self.driver.find_element(By.XPATH, self.search_xpath)
         global_search.send_keys(self.ritm)
@@ -70,13 +84,11 @@ class ScrapeRITM():
         return req, name, address
     
     def scrape_name(self):
-        # xpath of the container that holds all consultant info
-        consultant_info_xpath = '//tr[@id="element.container_23caec60e17c4a00c2ab91d15440c5ee"]'
         # xpath of first and last name child containers
         fn_xpath = '//tr[1]//div[@class="col-xs-12 form-field input_controls sc-form-field "]/input[1]'
         ln_xpath = '//tr[2]//div[@class="col-xs-12 form-field input_controls sc-form-field "]/input[1]'
-        name_xpaths = [f"{consultant_info_xpath}{fn_xpath}", 
-                            f"{consultant_info_xpath}{ln_xpath}"]
+        name_xpaths = [f"{self.consultant_info_xpath}{fn_xpath}", 
+                            f"{self.consultant_info_xpath}{ln_xpath}"]
         
         name = []
         # get the value from the xpath, strip any whitespace
@@ -90,7 +102,6 @@ class ScrapeRITM():
     
     def scrape_address(self):
         # xpath of container that holds all address info
-        address_info_xpath = '//tr[@id="element.container_66291a0ae1fc8a00c2ab91d15440c5c2"]'
         column_xpath1 = '//div[@class="section-content catalog-section-content"]/div[1]'
         column_xpath2 = '//div[@class="section-content catalog-section-content"]/div[2]'
         # xpath of street 1, street 2, and postal
@@ -99,25 +110,88 @@ class ScrapeRITM():
         postal_xpath = f'{column_xpath2}//tr[1]//div[@class="col-xs-12 form-field input_controls sc-form-field "]/input[1]'
 
         # order: ADDRESS 1, ADDRESS 2, POSTAL
-        address_xpaths = [f"{address_info_xpath}{street_one_xpath}",
-                          f"{address_info_xpath}{street_two_xpath}",
-                          f"{address_info_xpath}{postal_xpath}"]
+        address_xpaths = [f"{self.address_info_xpath}{street_one_xpath}",
+                          f"{self.address_info_xpath}{street_two_xpath}",
+                          f"{self.address_info_xpath}{postal_xpath}"]
 
         address = []
 
         for xpath in address_xpaths:
             element_xpath = self.driver.find_element(By.XPATH, xpath)
-            part = element_xpath.get_attribute("value")
-            part = part.strip()
-            if part != None or part == " ":
+            part = element_xpath.get_attribute("value").strip()
+
+            if part != None or part != " " or part != "":
                 address.append(part)
     
         return " ".join(address)
     
-    def scrape_req(self):
-        req_xpath = '//input[@id="sys_display.sc_req_item.request"]'
-        
-        element_xpath = self.driver.find_element(By.XPATH, req_xpath)
+    def scrape_req(self):        
+        element_xpath = self.driver.find_element(By.XPATH, self.req_xpath)
         req = element_xpath.get_attribute("value")
 
         return req
+    
+    def scrape_user_info(self):
+        # consultant's company xpaths
+        cid_xpath = '//tr[19]//input[@class="questionsetreference form-control element_reference_input"]'
+        oid_xpath = '//tr[24]//input[@class="questionsetreference form-control element_reference_input"]'
+        pid_xpath = '//tr[7]//input[@class="cat_item_option sc-content-pad form-control"]'
+        # CHECKS IF "NEW CUSTOMER" IS A VALUE ON CID XPATH.
+        # this is needed because if "New Customer" is selected then the company and CID xpaths
+        # swap positions with each other and every value below it increments by one.
+        customer_id_values = ["New Customer", "Not Listed"]
+        if self.driver.find_element(By.XPATH, f"{self.company_info_xpath}{cid_xpath}").get_attribute("value") in customer_id_values:
+            company_xpath = '//tr[21]//input[@class="cat_item_option sc-content-pad form-control"]'
+            cid_xpath = '//tr[22]//input[@class="cat_item_option sc-content-pad form-control"]'
+        else:
+            company_xpath = '//tr[21]//input[@class="cat_item_option sc-content-pad form-control"]'
+
+        # consultant xpaths
+        email_xpath = '//tr[3]//div[@class="col-xs-12 form-field input_controls sc-form-field "]/input[1]'
+        eid_xpath = '//tr[4]//div[@class="col-xs-12 form-field input_controls sc-form-field "]/input[1]'
+
+        consultant_xpaths = [email_xpath, eid_xpath]
+        user_info = []
+
+        for xpath in consultant_xpaths:
+            element_xpath = self.driver.find_element(By.XPATH, f"{self.consultant_info_xpath}{xpath}")
+            part = element_xpath.get_attribute("value")
+            user_info.append(part)
+
+        company_xpaths = [cid_xpath, company_xpath, oid_xpath, pid_xpath]
+
+        for xpath in company_xpaths:
+            element_xpath = self.driver.find_element(By.XPATH, f"{self.company_info_xpath}{xpath}")
+            part = element_xpath.get_attribute("value")
+
+            if part != None:
+                user_info.append(part)
+        
+        # returns a list in order: email, employee ID, customer ID, company, office ID, project ID.
+        return user_info
+        
+# fill in the user fields of a new user record
+# NOTE: still requires manual input in saving and other missing information
+# that requires interaction with the SNOW UI.
+class UserCreation:
+    def __init__(self, driver, link, user_info, name):
+        self.driver = driver
+        self.link = link
+        self.name = name
+
+        # company info, values are initialized from a list
+        self.email = user_info[0]
+        self.eid = user_info[1]
+        self.cid = user_info[2]
+        self.company = user_info[3]
+        self.oid = user_info[4]
+        self.pid = user_info[5]
+
+    def create_user(self):
+        self.driver.get(self.link)
+
+    def format_office_id(self):
+        full_oid = self.oid
+        full_oid = full_oid.split("-")
+
+        self.oid = full_oid[0]
