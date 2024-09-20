89f381c7852b992d093c6b7e02e37a8477a10446
Nguyen <tnguyen9@teksystems.com>
2024-09-20 15:40:17 -0500

    changed from selecting a list to working with one ritm at a time. included webdriverwait due to the constant changing between links. removed blacklist methods, instead use the one in the constructor.

--- core/vtb_scanner.py
+++ core/vtb_scanner.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 '''
 how the VTBScanner works:
@@ -21,60 +23,101 @@ class VTBScanner():
         self.vtb_link = link
         self.blacklist = set() if blacklist is None else blacklist
 
-    def add_blacklist(self, item):
-        self.blacklist.add(item)
-    
-    def clear_blacklist(self):
-        self.blacklist.clear()
-    
-    def return_blacklist(self):
-        return self.blacklist
+        self.req_lane = '//li[@v-lane-index="0" and @h-lane-index="0"]'
     
     def get_to_vtb(self) -> None:
+        '''
+        Sends the driver to the VTB directly.
+        '''
         self.driver.get(self.vtb_link)
-        time.sleep(10)
 
-    def get_ritm_numbers(self) -> list:
-        self.driver.switch_to.default_content() 
-        self.driver.switch_to.frame('gsft_main')
+    def get_ritm_number(self):
+        '''
+        Scans the request lane for a RITM number.
+
+        The scan starts from the top of the request lane.
+
+        It only returns a single RITM number. If the list is empty, then None is returned.
+        '''
+        self.driver.switch_to.default_content()
+        self.driver.switch_to.default_content()
+        WebDriverWait(self.driver, 15).until(
+            EC.frame_to_be_available_and_switch_to_it('gsft_main')
+        )
+
+        ritm = None
+        try:
+            ritm_elements = WebDriverWait(self.driver, 15).until(
+                EC.presence_of_all_elements_located((By.XPATH, f'{self.req_lane}//a[contains(text(), "RITM")]'))
+            )
+
+            if len(ritm_elements) > 0:
+                for element in ritm_elements:
+                    if element.text not in self.blacklist:
+                        ritm = element.text
+                        break
+        except TimeoutException:
+            pass
         
-        req_lane = '//li[@v-lane-index="0" and @h-lane-index="0"]'
-        ritm_elements = self.driver.find_elements(By.XPATH, f'{req_lane}//a[contains(text(), "RITM")]')
-
-        if len(ritm_elements) > 4:
-            ritm_elements = ritm_elements[:4]
-
-        ritm_numbers = []
-        if ritm_elements:
-            for element in ritm_elements:
-                if element not in self.blacklist:
-                    ritm_numbers.append(element.text)
-
         self.driver.switch_to.default_content()
 
-        return ritm_numbers
+        return ritm
 
-    def get_web_elements(self) -> list:
+    def get_ritm_element(self, ritm: str):
+        '''
+        Returns a RITM element.
+
+        If a RITM element is not found, it returns None.
+        '''
         # NOTE: this is going to be ran indefinitely as it will be scanning the VTB for any incoming tasks.
         # xpath to the lane which contains the items to look for.
-        self.driver.switch_to.frame('gsft_main')
+        self.driver.switch_to.default_content()
+        WebDriverWait(self.driver, 15).until(
+            EC.frame_to_be_available_and_switch_to_it('gsft_main')
+        )
         
-        req_lane = '//li[@v-lane-index="0" and @h-lane-index="0"]'
-        ritm_elements = self.driver.find_elements(By.XPATH, f'{req_lane}//a[contains(text(), "RITM")]')
-        inc_elements = self.driver.find_elements(By.XPATH, f'{req_lane}//a[contains(text(), "INC")]')
+        try:
+            ritm_elements = WebDriverWait(self.driver, 15).until(
+                EC.presence_of_all_elements_located((By.XPATH, f'{self.req_lane}//a[contains(text(), "{ritm}")]'))
+            )
+        except TimeoutException:
+            return None
         
-        # limit the list to a maximum of 4.
-        # NOTE: i do not know why it breaks when it is > 4, i can't be bothered.
-        if len(ritm_elements) > 4:
-            ritm_elements = ritm_elements[:4]
-        if len(inc_elements) > 4:
-            inc_elements = inc_elements[:4]
-
         self.driver.switch_to.default_content()
         
-        return ritm_elements, inc_elements
+        if len(ritm_elements) > 0:
+            return ritm_elements[0]
+
+    def get_inc_element(self) -> list:
+        '''
+        Returns an INC element.
+
+        If an INC element is not found, it returns None.
+        '''
+        self.driver.switch_to.default_content()
+        WebDriverWait(self.driver, 15).until(
+            EC.frame_to_be_available_and_switch_to_it('gsft_main')
+        )
+
+        try:
+            inc_elements = WebDriverWait(self.driver, 15).until(
+                EC.presence_of_all_elements_located((By.XPATH, f'{self.req_lane}//a[contains(text(), "INC")]'))
+            )
+        except TimeoutException:
+            return None
+        
+        if len(inc_elements) > 0:
+            return inc_elements[0]
     
-    def drag_task(self, elements: list, type: str):
+    def drag_task(self, element, type: str):
+        '''
+        Drags the task over to their respective lane. This does it for both INCs and RITMs.
+
+        INCs gets dragged to the ASAP/INC/Replace lane.
+
+        Most RITMs will get dragged over to the User Created lane, some may be in the Software row instead.
+        '''
+        self.driver.switch_to.default_content()
         self.driver.switch_to.frame('gsft_main')
 
         lane = self.driver.find_element(By.XPATH, '//li[@v-lane-index="1" and @h-lane-index="0"]')
@@ -85,7 +128,7 @@ class VTBScanner():
             inc_lane = self.driver.find_element(By.XPATH, '//li[@v-lane-index="2" and @h-lane-index="0"]')
             lane = inc_lane
 
-        for element in elements:
+        try:
             drag_task = ActionChains(self.driver).click_and_hold(element)
             time.sleep(1)
             drag_task.move_to_element(lane)
@@ -94,5 +137,7 @@ class VTBScanner():
 
             print('   Task dragged.')
             time.sleep(1.5)
+        except NoSuchElementException:
+            raise NoSuchElementException
         
         self.driver.switch_to.default_content()
