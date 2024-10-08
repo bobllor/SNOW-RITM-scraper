3c2f5efc653560e8217d313d3c8749cedc4dd0e3
Nguyen <tnguyen9@teksystems.com>
2024-10-08 09:43:05 -0500

    use of text format to format the texts in the program and init for package handling.

--- misc/text_formats.py
+++ misc/text_formats.py
@@ -1,10 +1,2 @@
-def element_text_format(element_list: list, task_type: str) -> str:
-    text = task_type
-
-    if len(element_list) > 1:
-        text = task_type + 's'
-    
-    if len(element_list) == 0:
-        text = f'no {task_type}s' 
-    
-    return text
+def text_format(text: str) -> str:
+    return f'   {text}'
